770b5776a2a0eddf3db14fe85c4cbe91ffaa1d4c
francois-tourigny <francois.tourigny@polymtl.ca>
2022-10-31 16:31:52 -0400

    changer var pour script

--- myscript.py
+++ myscript.py
@@ -1,6 +1,6 @@
 import os
 
 os.system('git bisect reset')
-os.system('git bisect start $badhash $goodhash')
+os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c0')
 os.system('git bisect run manage.py test')
 os.system('git bisect system')
