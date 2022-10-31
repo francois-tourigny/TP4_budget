4a2991ef0e267c6a6352a305395d917e08221a30
francois-tourigny <francois.tourigny@polymtl.ca>
2022-10-31 16:54:19 -0400

    change reset of bisect

--- myscript.py
+++ myscript.py
@@ -2,7 +2,6 @@ import os
 
 os.system('good_commit=200~e4cfc6f77ebbe2e23550ddab682316ab4ce1c0')
 os.system('bad_commit=c1a4be04b972b6c17db242fc37752ad517c29402')
-os.system('git bisect reset')
 os.system('git bisect start $bad_commit $good_commit')
 os.system('git bisect run manage.py test')
 os.system('git bisect reset')
